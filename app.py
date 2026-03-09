68a32139cb4006c27244cefe918af23853ec82e7
Carlos González-ai <carlos30roberto3@gmail.com>
2026-03-09 01:44:26 -0600

    agregar ruta principal

--- app.py
+++ app.py
@@ -207,3 +207,11 @@ if __name__ == "__main__":
 
 port = int(os.environ.get("PORT", 8080))
 app.run(host="0.0.0.0", port=port)
+from flask import Flask, render_template
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+    return render_template("inventario.html")
